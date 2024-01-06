b081956c16e7fe62eff6261a305bb3d9eed5adc0
Jordan <jlprogrammer_98@hotmail.com>
2024-01-06 21:09:50 +0000

    posting data

--- src/routes/gptHandler.js
+++ src/routes/gptHandler.js
@@ -1,8 +1,22 @@
-const express = require('express')
+const express = require("express");
+const multer = require('multer')
 const router = new express.Router();
 
-router.get('/', (req, res) => {
-    res.render('home');
-})
+router.get("/gpt-form", (req, res) => {
+  res.render("home");
+});
+
+router.post("/gpt-form", multer().none(), (req, res, next) => {
+    //receieve and parse incoming request data
+    if(!req.body.userInput|| req.body.userInput.length === 0) {
+        res.status(404).send('Cannot Post Data');
+        next();
+    }
+    //send fetch request to openAI
+
+    //error handle the promise
+
+    //render the page with new data
+});
 
 module.exports = router;
