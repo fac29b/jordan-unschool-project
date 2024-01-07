8e84203d9bdb769014e94ac60060b1bc493cb29c
Jordan <jlprogrammer_98@hotmail.com>
2024-01-07 15:55:23 +0000

    using env variable for tokens

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require('dotenv').config();
 const bodyParser = require('body-parser');
 const path = require("path");
 const server_port = process.env.PORT || 3001;
@@ -19,7 +20,7 @@ app.use(bodyParser.json())
 
 const gptHandler = require('./routes/gptHandler')
 
-//routes
+//register routes
 app.use(gptHandler);
 
 app.listen(server_port, () => {
